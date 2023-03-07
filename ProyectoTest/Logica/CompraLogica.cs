648429668a58f19fdb4152d1f8a2dcf655a2e0f4
cristian castro <cristianfelipecastro693@gmail.com>
2023-03-07 13:24:20 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoTest/Logica/CompraLogica.cs
@@ -0,0 +1,76 @@
+﻿using ProyectoTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace ProyectoTest.Logica
+{
+    public class CompraLogica
+    {
+        private static CompraLogica _instancia = null;
+
+        public CompraLogica()
+        {
+
+        }
+
+        public static CompraLogica Instancia
+        {
+            get
+            {
+                if (_instancia == null)
+                {
+                    _instancia = new CompraLogica();
+                }
+
+                return _instancia;
+            }
+        }
+
+        public bool Registrar(Compra oCompra)
+        {
+            oCompra.Contacto = "3209066384";
+            oCompra.Telefono = "3209066384";
+            oCompra.Direccion = "Cll 25 #37A-14";
+            bool respuesta = false;
+            using (SqlConnection oConexion = new SqlConnection(Conexion.CN))
+            {
+                try
+                {
+                    StringBuilder query = new StringBuilder();
+                    foreach (DetalleCompra dc in oCompra.oDetalleCompra) {
+                        query.AppendLine("insert into detalle_compra(IdCompra,IdProducto,Cantidad,Total) values (¡idcompra!," + dc.IdProducto +","+dc.Cantidad+","+dc.Total+")");
+                    }
+
+                    SqlCommand cmd = new SqlCommand("sp_registrarCompra", oConexion);
+                    cmd.Parameters.AddWithValue("IdUsuario", oCompra.IdUsuario);
+                    cmd.Parameters.AddWithValue("TotalProducto", oCompra.TotalProducto);
+                    cmd.Parameters.AddWithValue("Total", oCompra.Total);
+                    cmd.Parameters.AddWithValue("Contacto", oCompra.Contacto);
+                    cmd.Parameters.AddWithValue("Telefono", oCompra.Telefono);
+                    cmd.Parameters.AddWithValue("Direccion", oCompra.Direccion);
+                    cmd.Parameters.AddWithValue("QueryDetalleCompra", query.ToString());
+                    cmd.Parameters.Add("Resultado", SqlDbType.Bit).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    oConexion.Open();
+                    cmd.ExecuteNonQuery();
+                    respuesta = Convert.ToBoolean(cmd.Parameters["Resultado"].Value);
+
+                }
+                catch (Exception ex)
+                {
+                    respuesta = false;
+                }
+            }
+            return respuesta;
+        }
+
+
+
+    }
+}
---
+++ ProyectoTest/Logica/Conexion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoTest.Logica
+{
+    public class Conexion
+    {
+       //public static string CN = "Data Source=SQL5101.site4now.net;Initial Catalog=db_a8f842_dbcarritoxpay;User Id=db_a8f842_dbcarritoxpay_admin;Password=Felipe&10";
+        //public static string CN = "workstation id=DBCARRITOXPAY.mssql.somee.com;packet size=4096;user id=xpay_practica_SQLLogin_1;pwd=oqwvnyypnu;data source=DBCARRITOXPAY.mssql.somee.com;persist security info=False;initial catalog=DBCARRITOXPAY";
+        //public static string CN = "Data Source=DBCARRITOXPAY.mssql.somee.com;Initial Catalog=DB_CARRITO;user id=xpay_practica_SQLLogin_1;pwd=oqwvnyypnu";
+        //public static string CN = "Data Source=.;Initial Catalog=DB_CARRITO;Integrated Security=True";
+        public static string CN = "Data Source=.;Initial Catalog=DB_CARRITO;Integrated Security=True";
+    }
+}
